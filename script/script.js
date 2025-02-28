fcdf13aa117bec1c696729087fa9f57aa3295ad5
ratulislam46 <arfanratul46@gmail.com>
2025-02-28 10:07:18 +0600

    added wrist size

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const ringButtons = document.querySelectorAll('.ring-button');
 for (let i = 0; i < ringButtons.length; i++){
     const ringBtn = ringButtons[i];
+    
     ringBtn.addEventListener("click", function(event){
         //click bottons and change 
         const color = event.target.id.replace("-color", "")
@@ -9,6 +10,7 @@ for (let i = 0; i < ringButtons.length; i++){
             ringButtons[j].classList.remove('border-purple-600');
             ringButtons[j].classList.add('border-gray-300');
         }
+
         event.target.classList.add("border-purple-600")
         event.target.classList.remove("border-gray-300")
 
@@ -17,4 +19,19 @@ for (let i = 0; i < ringButtons.length; i++){
         productImage.src = "../images/" + color + ".png"
     })
 }
+
+function selectWristSize(size) {
+    const sizes = ["S", "M", "L", "XL"];
+    for (let i = 0; i < sizes.length; i++) {
+      const button = document.getElementById("size-" + sizes[i]);
+      const elemnt = sizes[i];
+      if (size === elemnt) {
+        button.classList.add("border-purple-600");
+      } else {
+        button.classList.remove("border-purple-600");
+      }
+    }
+  }
+
+
 
