f7de271caed260e37fa4da7a54077e5ac2635569
ratulislam46 <arfanratul46@gmail.com>
2025-02-28 09:30:16 +0600

    botton click option done

---
+++ script/script.js
@@ -0,0 +1,20 @@
+const ringButtons = document.querySelectorAll('.ring-button');
+for (let i = 0; i < ringButtons.length; i++){
+    const ringBtn = ringButtons[i];
+    ringBtn.addEventListener("click", function(event){
+        //click bottons and change 
+        const color = event.target.id.replace("-color", "")
+
+        for(let j = 0; j< ringButtons.length; j++){
+            ringButtons[j].classList.remove('border-purple-600');
+            ringButtons[j].classList.add('border-gray-300');
+        }
+        event.target.classList.add("border-purple-600")
+        event.target.classList.remove("border-gray-300")
+
+        const productImage = document.getElementById("product-image");
+        // productImage.src = '../images/teal.png';
+        productImage.src = "../images/" + color + ".png"
+    })
+}
+
